aedd7c09d974c245987e6800397499bc7ce287c2
bilevych_yv <ybilevich@gmail.com>
2023-05-30 11:42:54 +0300

    add paginator part1

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 from .models import Product
 from category.models import Category
 from carts.models import CartItem
@@ -15,10 +17,13 @@ def store(request, category_slug=None):
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 6) # Show 6 products per page.
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
